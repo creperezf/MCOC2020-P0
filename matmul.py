389b9edf65833408568b13a93f3c3ba09aa7c478
creperezf <69158551+creperezf@users.noreply.github.com>
2020-08-05 14:55:50 -0400

    Add files via upload

---
+++ matmul.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  4 20:06:56 2020
+
+@author: TOSHIBA 2IN1
+"""
+
+import scipy as sp
+
+N = 3
+
+A = sp.matrix(sp.rand(N,N))
+B = sp.matrix(sp.rand(N,N))
+
+print(f"A = \n{A}")
+print(f"B = \n{B}")
+
+C = A*B     # Ojo numpy esto es multiplicacion de arreglos 
+
+
+
+
+print(f"C = \n{C}")
+
+print(f"C00 = \n{C[0,0]}")
+print(f"A00 * B00 = \n{A[0,0]*B[0,0]}")
