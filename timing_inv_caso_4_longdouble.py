fef92f1e63fd0b10d06ed8bbce07c082390cb012
creperezf <69158551+creperezf@users.noreply.github.com>
2020-08-12 22:57:05 -0400

    Update timing_inv_caso_4_longdouble.py

--- timing_inv_caso_4_longdouble.py
+++ timing_inv_caso_4_longdouble.py
@@ -13,6 +13,7 @@ from funcionlaplace import matriz_laplaciana3
 lista=[]
 listamem=[]
 lista2=[]
+lista3=[]
 Ns = [1, 2, 3, 4, 6, 8, 10, 12, 14, 16, 18, 20, 25, 30, 35,40,45, 50, 60,70, 80, 90, 100,200,500,1000,2000,5000]
 for N in Ns:
     Z = matriz_laplaciana3(N)
@@ -31,17 +32,18 @@ for N in Ns:
     dt = t2 - t1
     ds = t4 - t3
     dr = t6 - t5
+    size=3*(N**2)*8 
         
     lista.append(dt)
     listamem.append(ds)
     lista2.append(dr)
-        
+    lista3.append(size)   
     x=["","","","","","","10","","","","","20","","","","","","50","","","","","100","200","500","1000","2000","5000"]
     x1=["","","","","","","","","","","","",""]
     y=[0.1e-3,1e-3,1e-2,0.1,1.,10,60,60*10,60*60]
     ejey=["0.1 ms","1 ms", "10 ms", "0.1 s", "1 s", "10 s", "1 min","10 min","1 h"]
-    Memy=[0.1e-3,1e-3,1e-2,0.1,1.,10,60,60*10,60*60]
-    Memy2=["0.1 ms","1 ms", "10 ms", "0.1 s", "1 s", "10 s", "1 min","10 min","1 h"]
+    Memy=[10**3,10**4,10**5,10**6,10**7,10**8,10**9,10**10]
+    Memy2=["1 KB","10 KB","100 KB","1 MB","10 MB","100 MB","1 GB","10 GB"]
 
 pyplot.subplot(2,1,1)
 pyplot.loglog(Ns,lista,"-o")
@@ -51,19 +53,18 @@ pyplot.grid(True)
 
 pyplot.loglog(Ns,listamem,"-o")
 pyplot.xticks(Ns,x1)
-pyplot.yticks(Memy,Memy2)
+pyplot.yticks(y,ejey)
 pyplot.grid(True)
 
 pyplot.loglog(Ns,lista2,"-o")
-pyplot.xticks(Ns,x,rotation=45)
 pyplot.yticks(y,ejey) 
 pyplot.grid(True)
-
+pyplot.xticks(Ns,x1)
 pyplot.ylabel("tiempo transcurrido")
-pyplot.title("Rendimiento matriz inversa A (single)")
+pyplot.title("Rendimiento matriz inversa A (longdouble)")
 pyplot.legend(["Numpylinalg","scipylinalg","scipylinalg overwrite"])
 pyplot.subplot(2,1,2)
-pyplot.loglog(Ns,listamem,"-o")
+pyplot.loglog(Ns,lista3,"-o")
 pyplot.xticks(Ns,x,rotation=45)
 pyplot.yticks(Memy,Memy2)
 pyplot.xlabel("tamaño de la matriz")
